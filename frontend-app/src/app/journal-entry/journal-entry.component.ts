b7a30b70213ea31bb2e13986eda377f1fa3b24b8
grace-adeb <boluwatife.adebowale@st-annes.ox.ac.uk>
2023-10-27 23:09:45 +0100

    changed journal-entry ts to use PostDetail import

--- frontend-app/src/app/journal-entry/journal-entry.component.ts
+++ frontend-app/src/app/journal-entry/journal-entry.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PostDetail } from '../postdetail';
 
 @Component({
   selector: 'app-journal-entry',
   templateUrl: './journal-entry.component.html',
-  styleUrls: ['./journal-entry.component.css']
+  styleUrls: ['./journal-entry.component.css'],
+  standalone: true
 })
-export class JournalEntryComponent {
 
+export class JournalEntryComponent {
+  @Input() entryDetail!: PostDetail;
 }
+
