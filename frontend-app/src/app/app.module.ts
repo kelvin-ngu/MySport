aba9cb41030a8fb43f805228c4ad00c458e2c192
kbuaaaaaa <karnbongkot@gmail.com>
2023-10-27 20:34:27 +0100

    Extract home component

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
