b6bdd410121cfbdac1e6ded1b076d7a9b5aea312
Joshua Adebayo <ayoade.josh@gmail.com>
2023-10-27 22:05:54 +0100

    MyGame
    I have made mygame componet

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { RouterModule } from '@angular/router';
+import { MygameComponent } from './mygame/mygame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MygameComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
+      {path: 'mygame', component: MygameComponent},
     ]),
   ],
   providers: [],
---
+++ frontend-app/src/app/mygame/mygame.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mygame',
+  templateUrl: './mygame.component.html',
+  styleUrls: ['./mygame.component.css']
+})
+export class MygameComponent {
+
+}
