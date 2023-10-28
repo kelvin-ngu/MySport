e03377e5864671580e12d670184d99bebfef6c8c
Hashim Iqbal <iqbalhashim332@gmail.com>
2023-10-28 05:56:53 +0100

    Fixed issue with routing links not working

--- frontend-app/src/app/community/community.component.ts
+++ frontend-app/src/app/community/community.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, resolveForwardRef } from '@angular/core';
 import { PostComponent } from '../post/post.component';
 import { TabbedContentComponent } from '../tabbed-content/tabbed-content.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-community',
@@ -11,8 +12,9 @@ import { TabbedContentComponent } from '../tabbed-content/tabbed-content.compone
 })
 
 export class CommunityComponent {
+  constructor(private router : Router) {}
 
-  constructor(){
-
+  navigateTo(route: string) {
+    this.router.navigate([route]);
   }
 }
