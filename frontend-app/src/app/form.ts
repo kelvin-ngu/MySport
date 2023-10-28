27b6b84b1f5917427502f72a868c5a22c861fc94
grace-adeb <boluwatife.adebowale@st-annes.ox.ac.uk>
2023-10-28 09:47:53 +0100

    entries components made

--- frontend-app/src/app/form.ts
+++ frontend-app/src/app/form.ts
@@ -1,2 +1,11 @@
+import { Player } from "./player";
+
 export interface Form {
+    id: string;
+    player: Player,
+
+    rating: number,
+    match_description: string,
+    comment: string,
+    created_at: string
 }
