3cf4bf5b1a773eb0d3f58faeda9a2eb15ac40e25
Kelvin <kelvinngu0908@gmail.com>
2023-10-28 09:38:42 +0100

    Update forum urls

--- PlayerVoice/forum/urls.py
+++ PlayerVoice/forum/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('', views.PostViewSet, basename='post')
+router.register('post', views.PostViewSet, basename='post')
 
 urlpatterns = [
-    path('post/', include(router.urls)),
+    path('', include(router.urls)),
 ]
